97baf46e0e153e85956e0910899ac524a2202fa3
likeyu <1678667175@qq.com>
2019-06-14 20:00:42 +0800

    程序入口(测试目录完整)

---
+++ src/com/lky/main/Main.java
@@ -0,0 +1,11 @@
+package com.lky.main;
+
+/**
+ * @auther likeyu
+ * @create 2019-06-14-19:58
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("测试github的项目结构是否同步");
+    }
+}
